8d3b093e1d7450963e1e58388eb2e1cdb7abd853
Ayaka Yasuda <ayakayasuda23@gmail.com>
2022-01-19 15:49:44 -0800

    added a form to create a new user

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -1,11 +1,32 @@
 // index.js executes functions when the page is loaded
 
 const indexModule = (() => {
-  // add an event listener that occurs when you click the search button
-  document.getElementById('search-btn').addEventListener('click', () => {
-    return searchModule.searchUsers();
-  });
+  // get a current path
+  const path = window.location.pathname;
 
-  // execute fetchAllUsers method that usersModule has
-  return usersModule.fetchAllUsers();
+  switch (path) {
+    case '/':
+      // add an event listener that occurs when you click the search button
+      document.getElementById('search-btn').addEventListener('click', () => {
+        return searchModule.searchUsers();
+      });
+
+      // execute fetchAllUsers method that usersModule has
+      return usersModule.fetchAllUsers();
+
+    case '/create.html':
+      // add an event listener that occurs when you click the save button
+      document.getElementById('save-btn').addEventListener('click', () => {
+        return usersModule.createUsers();
+      });
+
+      // add an event listener that occurs when you click the cancel button
+      document.getElementById('cancel-btn').addEventListener('click', () => {
+        return (window.location.href = '/');
+      });
+      break;
+
+    default:
+      break;
+  }
 })();
--- app/public/js/users.js
+++ app/public/js/users.js
@@ -4,6 +4,10 @@
 const usersModule = (() => {
   const BASE_URL = 'http://localhost:3000/api/v1/users';
 
+  // set up header
+  const headers = new Headers();
+  headers.set('Content-Type', 'application/json');
+
   return {
     fetchAllUsers: async () => {
       const res = await fetch(BASE_URL);
@@ -25,6 +29,29 @@ const usersModule = (() => {
           .getElementById('users-list')
           .insertAdjacentHTML('beforeend', body);
       }
+    },
+
+    createUsers: async () => {
+      const name = document.getElementById('name').value;
+      const profile = document.getElementById('profile').value;
+      const dateOfBirth = document.getElementById('date-of-birth').value;
+
+      // body of request
+      const body = {
+        name: name,
+        profile: profile,
+        date_of_birth: dateOfBirth,
+      };
+
+      const res = await fetch(BASE_URL, {
+        method: 'POST',
+        headers: headers,
+        body: JSON.stringify(body),
+      });
+      const resJSON = await res.json();
+
+      alert(resJSON.message);
+      window.location.href = '/';
     },
   };
 })();
